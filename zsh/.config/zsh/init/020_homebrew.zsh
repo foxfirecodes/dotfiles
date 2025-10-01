bcab1e89c039b84569d6e9ae7c0779493955ef53
foxfirecodes <foxfirecodes@gmail.com>
2025-10-01 13:58:37 -0400

    [zsh] add homebrew zsh completions when available

--- zsh/.config/zsh/init/020_homebrew.zsh
+++ zsh/.config/zsh/init/020_homebrew.zsh
@@ -1,3 +1,6 @@
 if [ -d /opt/homebrew/bin ]; then
   add_to_path /opt/homebrew/bin prepend
 fi
+if [ -d /opt/homebrew/share/zsh/site-functions ]; then
+  fpath+=(/opt/homebrew/share/zsh/site-functions)
+fi
