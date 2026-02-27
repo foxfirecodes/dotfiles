5a9506556ba67b3ae9acc8d89e3c09c51cd87a47
foxfirecodes <foxfirecodes@gmail.com>
2026-02-27 18:58:30 -0500

    [nvim] cleanup astrocore

--- nvim/.config/nvim/lua/plugins/astrocore.lua
+++ nvim/.config/nvim/lua/plugins/astrocore.lua
@@ -6,14 +6,30 @@ return {
     autocmds = {
       tsdenofix = {
         event = { "LspAttach" },
-	      desc = "Fix ts_ls & deno lsp conflict",
-	      callback = function(ev)
+        desc = "Fix ts_ls & deno lsp conflict",
+        callback = function(ev)
           local client = vim.lsp.get_client_by_id(ev.data.client_id)
           if client and client.name == "ts_ls" then
-            local is_deno = not (vim.fn.findfile("deno.json", ".;") == '')
+            local is_deno = not (vim.fn.findfile("deno.json", ".;") == "")
             if is_deno then client.stop() end
           end
         end,
+      },
+      quickfix_close_on_select = {
+        {
+          -- trigger on entering quickfix filetype buffer
+          event = "FileType",
+          pattern = "qf",
+          desc = "Set 'o' to select item and close quickfix",
+          callback = function(args)
+            -- Set buffer-local keymap for 'o'
+            vim.keymap.set("n", "o", "<CR>:cclose<CR>", {
+              buffer = args.buf,
+              desc = "Select and close quickfix",
+              remap = true,
+            })
+          end,
+        },
       },
     },
   },
