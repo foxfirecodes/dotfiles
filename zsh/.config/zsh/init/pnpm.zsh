ab1cc95397faab5bc68424c3dff8e26de9fb0756
foxfirecodes <foxfirecodes@gmail.com>
2026-03-06 17:57:52 -0500

    [pnpm] handle macos pnpm_home

--- zsh/.config/zsh/init/pnpm.zsh
+++ zsh/.config/zsh/init/pnpm.zsh
@@ -1,4 +1,7 @@
 if [ -d ~/.local/share/pnpm ]; then
   export PNPM_HOME="$HOME/.local/share/pnpm"
   add_to_path "$PNPM_HOME"
+elif [ -d ~/Library/pnpm ]; then
+  export PNPM_HOME="$HOME/Library/pnpm"
+  add_to_path "$PNPM_HOME"
 fi
