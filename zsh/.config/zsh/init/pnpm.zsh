d8f9f55d559aa19d45aa3916cd27b080e71f0cef
foxfirecodes <foxfirecodes@gmail.com>
2026-06-02 11:39:58 -0400

    [zsh] use new pnpm bin path

--- zsh/.config/zsh/init/pnpm.zsh
+++ zsh/.config/zsh/init/pnpm.zsh
@@ -1,7 +1,9 @@
 if [ -d ~/.local/share/pnpm ]; then
   export PNPM_HOME="$HOME/.local/share/pnpm"
   add_to_path "$PNPM_HOME"
+  add_to_path "$PNPM_HOME/bin"
 elif [ -d ~/Library/pnpm ]; then
   export PNPM_HOME="$HOME/Library/pnpm"
   add_to_path "$PNPM_HOME"
+  add_to_path "$PNPM_HOME/bin"
 fi
