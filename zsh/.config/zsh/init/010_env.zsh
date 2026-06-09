d5a3a3ec7ecf79c5e0d123417005b570c363a2f3
foxfirecodes <foxfirecodes@gmail.com>
2026-06-08 22:34:51 -0400

    [zsh] dont add ./node_modules/.bin into path

--- zsh/.config/zsh/init/010_env.zsh
+++ zsh/.config/zsh/init/010_env.zsh
@@ -13,7 +13,6 @@ export GPG_TTY=$(tty)
 
 add_to_path "$HOME/.local/bin"
 add_to_path "$HOME/.bin"
-add_to_path "./node_modules/.bin"
 add_to_path "./target/debug"
 
 if [ -f "/etc/wsl.conf" ]; then
