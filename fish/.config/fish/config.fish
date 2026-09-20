727ea57697bd7be18033f533ce47b33b7cd38bd1
foxfirecodes <foxfirecodes@gmail.com>
2026-09-20 18:30:08 -0400

    [fish] starship + conditional cachyos

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,8 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
 source (status dirname)/git-aliases.fish
+
+if test -f /usr/share/cachyos-fish-config/cachyos-config.fish
+  source /usr/share/cachyos-fish-config/cachyos-config.fish
+end
 
 bind ctrl-space accept-autosuggestion
 bind ctrl-o 'tmux at || tmux new-session -s "$(basename "$PWD")"' repaint
@@ -8,6 +11,10 @@ bind ctrl-a 'tmux at' repaint
 
 if type -q zoxide
   zoxide init fish | source
+end
+
+if type -q starship
+  source (starship init fish --print-full-init | psub)
 end
 
 # overwrite greeting
